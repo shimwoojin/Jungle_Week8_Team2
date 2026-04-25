b50951fc52101e79e1aed10d44b8636aaecee9a3
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-04-25 19:08:41 +0900

    Add: ShadowAtlasQuadTree class

---
+++ KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.cpp
@@ -0,0 +1,134 @@
+﻿#include "ShadowAtlasQuadTree.h"
+#include "Render/Types/ForwardLightData.h"
+
+// Public functions
+void FShadowAtlasQuadTree::Init(float InAtlasSize, float InMinShadowMapResolution) {
+	if (InMinShadowMapResolution <= 0.f || InAtlasSize <= 0.f) {
+		return;
+	}
+	if (InMinShadowMapResolution > InAtlasSize) {
+		return;
+	}
+
+	AtlasSize = InAtlasSize;
+	MinShadowMapResolution = InMinShadowMapResolution;
+	Node RootNode = {};
+	RootNode.TopLeft = FVector2(0.f, 0.f);
+	RootNode.Resolution = InAtlasSize;
+
+	Nodes.push_back(RootNode);
+}
+
+FAtlasRegion FShadowAtlasQuadTree::Add(const FLightInfo& InLightInfo) {
+	if (Nodes.empty()) return { 0, 0, 0, false };
+
+	uint32 RequestedSize = static_cast<uint32>(EvaluateResolution(InLightInfo));
+	return AllocateNode(0, RequestedSize);
+}
+
+void FShadowAtlasQuadTree::Reset() {
+	if (!Nodes.empty()) {
+		Nodes.resize(1);
+		Node& Root = Nodes[0];
+		Root.bOccupied = false;
+		Root.bSplit = false;
+		Root.Children[0] = Root.Children[1] = Root.Children[2] = Root.Children[3] = -1;
+	}
+}
+
+void FShadowAtlasQuadTree::Clear() {
+	Nodes.clear();
+}
+
+// Private helpers
+FAtlasRegion FShadowAtlasQuadTree::AllocateNode(int32 NodeIdx, uint32 RequestedSize) {
+	if (NodeIdx < 0
+		|| NodeIdx >= Nodes.size()
+		|| Nodes[NodeIdx].bOccupied) {
+		// Invalid Node index
+		return { 0, 0, 0, false };
+	}
+
+	Node node = Nodes[NodeIdx];
+	if (node.bSplit) {
+		for (int32 SubIdx : node.Children) {
+			// Greedily allocate the first child node that can fit the requested size
+			FAtlasRegion AllocatedRegion = AllocateNode(SubIdx, RequestedSize);
+			if (AllocatedRegion.bValid) {
+				return AllocatedRegion;
+			}
+		}
+
+		return { 0, 0, 0, false };
+	}
+	else {
+		if (node.Resolution == RequestedSize) {
+			Nodes[NodeIdx].bOccupied = true;
+			return { static_cast<uint32> (node.TopLeft.X), static_cast<uint32> (node.TopLeft.Y), static_cast<uint32>(node.Resolution), true };
+		}
+		else {
+
+			// Try again after splitting
+			if (Split(NodeIdx)) {
+				return AllocateNode(NodeIdx, RequestedSize);
+			}
+		}
+	}
+
+	return { 0, 0, 0, false };
+}
+
+bool FShadowAtlasQuadTree::Split(int32 Idx) {
+	if (Idx < 0 || Idx >= (int32)Nodes.size()) return false;
+
+	float HalfRes = Nodes[Idx].Resolution / 2.f;
+	if (HalfRes < MinShadowMapResolution) {
+		// Reached the minimum resolution. Should not split further.
+		return false;
+	}
+
+	FVector2 TL = Nodes[Idx].TopLeft;
+	for (int i = 0; i < 4; i++) {
+		Node NewNode = {};
+		NewNode.Resolution = HalfRes;
+
+		switch (i) {
+		case (0): // Top-Left
+			NewNode.TopLeft = TL;
+			break;
+		case (1): // Top-Right
+			NewNode.TopLeft = TL + FVector2(HalfRes, 0);
+			break;
+		case (2): // Bottom-Left
+			NewNode.TopLeft = TL + FVector2(0, HalfRes);
+			break;
+		case (3): // Bottom-Right
+			NewNode.TopLeft = TL + FVector2(HalfRes, HalfRes);
+			break;
+		}
+		Nodes.push_back(NewNode);
+		Nodes[Idx].Children[i] = Nodes.size() - 1;
+	}
+	Nodes[Idx].bSplit = true;
+	return true;
+}
+
+float FShadowAtlasQuadTree::EvaluateResolution(const FLightInfo& InLightInfo) const {
+	FVector4 Color = InLightInfo.Color;
+	float    Intensity = InLightInfo.Intensity;
+	float    Radius = InLightInfo.AttenuationRadius;
+	float    Falloff = InLightInfo.FalloffExponent;
+
+	// Spotlight evaluation
+	float Score = 0.f;
+	Score += Color.X * 0.2126f + Color.Y * 0.7152f + Color.Z * 0.0722f; // Luminance
+	Score *= Intensity;
+	Score *= Radius;
+	Score += pow(Falloff, 2.f);
+
+	uint32 res = 64;
+	if (Score > 50.f)  res = 128;
+	if (Score > 200.f) res = 256;
+	if (Score > 800.f) res = 512;
+	return res;
+}
---
+++ KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include "Math/Vector.h"
+#include "Core/ClassTypes.h"
+
+class UCameraComponent;
+
+// Exclusive to spotlights
+struct FLightInfo;
+
+//enum class EShadowMapSizeUpperBound : uint8 {
+//	Size64		= 64,
+//	Size128		= 128,
+//	Size256		= 256,
+//	Size512		= 512,
+//	Size1024	= 1024,
+//	Size2048	= 2048
+//};
+
+struct Node {
+	// Dimensions
+	FVector2		TopLeft;
+	float			Resolution;
+
+	// Data
+	bool			bOccupied = false;	// True if and only if this node is allocated to a shadow map by whole
+	bool			bSplit = false;  // True if and only if this node has been split into a subtree
+	int32			Children[4] = { -1, -1, -1, -1 };		// indices into Nodes[], -1 = no child
+};
+
+struct FAtlasRegion { uint32 X, Y, Size; bool bValid; };
+
+// Buddy allocation quadtree for shadow atlas management
+class FShadowAtlasQuadTree {
+public:
+	// Initializes the head node of the atlas, and define the minimum resolution for the shadow maps to be allocated.
+	void Init(float InAtlasSize, float inMinShadowMapResolution);
+
+	// Try allocating the uv region for the input light source
+	FAtlasRegion Add(const FLightInfo& InLightInfo);
+
+	// Called every frame to reset the atlas.
+	void Reset();
+
+	// Hard clear including the root node
+	void Clear();
+
+	float GetAtlasSize() const { return AtlasSize; }
+
+private:
+	// Allocates the node at NodeIdx and returns the corresponding atlas region. Returns invalid region if the node is occupied or too small.
+	FAtlasRegion AllocateNode(int32 NodeIdx, uint32 RequestedSize);
+
+	// Greedily splits the quadtree to find the best fit for the new shadow map
+	bool  Split(int32 Idx);
+
+	// Ranks the importance of the input light source based on its properties.
+	float EvaluateResolution(const FLightInfo& InLightInfo) const;
+
+
+private:
+	TArray<Node>	  Nodes;
+
+	float AtlasSize = 4096.f;
+	float MinShadowMapResolution = 64.f;
+};
