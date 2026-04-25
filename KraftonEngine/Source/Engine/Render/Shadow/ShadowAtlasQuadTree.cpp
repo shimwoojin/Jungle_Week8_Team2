d6c9b21dc5343680e8c516a44f678d2804f7c442
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-04-25 13:47:22 +0900

    Add Init function to ShadowAtlasQuadTree class

--- KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.cpp
+++ KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.cpp
@@ -1,5 +1,42 @@
 ﻿#include "ShadowAtlasQuadTree.h"
+#include "Render/Pipeline/ForwardLightData.h"
+
+// Public functions
+void FShadowAtlasQuadTree::Init(float InAtlasSize, float InMinShadowMapResolution) {
+	if (InMinShadowMapResolution <= 0.f || InAtlasSize <= 0.f) {
+		return;
+	}
+	if (InMinShadowMapResolution < InAtlasSize) {
+		return;
+	}
+
+	MinShadowMapResolution = InMinShadowMapResolution;
+	Node RootNode		= {};
+	RootNode.TopLeft	= FVector2(0.f, 0.f);
+	RootNode.Resolution = InAtlasSize;
+}
+
+void FShadowAtlasQuadTree::Add(FLightInfo& InLightInfo) {
+	// First check if there is a node of fitting size.
+	
+
+}
 
 void FShadowAtlasQuadTree::Clear() {
-	Nodes.empty();
+
+}
+
+// Private helpers
+void FShadowAtlasQuadTree::SplitQuadTree() {
+
+}
+
+float FShadowAtlasQuadTree::EvaluateLightImportance(const FLightInfo& InLightInfo) {
+	FVector  Direction = InLightInfo.Direction;
+	FVector4 Color	   = InLightInfo.Color;
+	float    Intensity = InLightInfo.Intensity;
+	float    Radius    = InLightInfo.AttenuationRadius; 
+	float    Falloff   = InLightInfo.FalloffExponent;
+
+	return 0.f;
 }
--- KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.h
+++ KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.h
@@ -2,23 +2,39 @@
 #include "Math/Vector.h"
 #include "Core/ClassTypes.h"
 
+#include <queue>
+
 // Exclusive to spotlights
 struct FLightInfo;
+
+//enum class EShadowMapSizeUpperBound : uint8 {
+//	Size64		= 64,
+//	Size128		= 128,
+//	Size256		= 256,
+//	Size512		= 512,
+//	Size1024	= 1024,
+//	Size2048	= 2048
+//};
 
 struct Node {
 	// Dimensions
 	FVector2		TopLeft;
-	float			Resolution;		// Minimum resolution = 64 x 64
+	float			Resolution;			// Minimum resolution = 64 x 64
 
 	// Data
 	bool			bOccupied = false;	// True if and only if this node is allocated to a shadow map by whole
-	TArray<Node>	Children;
+	bool			bSplit    = false;
+	int32			Children[4] = {-1, -1, -1, -1};		// indices into Nodes[], -1 = no child
 };
 
 // Buddy allocation quadtree for shadow atlas management
 class FShadowAtlasQuadTree {
 public:
+	void Init(float InAtlasSize, float inMinShadowMapResolution);
+
+	// Try allocating the uv region for the input light source
 	void Add(FLightInfo& InLightInfo);
+
 	void Clear();
 
 private:
@@ -30,6 +46,8 @@ private:
 
 
 private:
+	std::queue<Node> NodeQueue;	// Queue for breadth-first traversal of the quadtree
 	TArray<Node> Nodes;
 	
+	float MinShadowMapResolution;
 };
