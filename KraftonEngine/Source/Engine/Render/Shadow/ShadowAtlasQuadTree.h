4e5099cf77056a49e0c82823dae24a69f05c287e
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-04-25 11:30:01 +0900

    checkpoint

---
+++ KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Math/Vector.h"
+#include "Core/ClassTypes.h"
+
+struct Node {
+	
+};
+
+class FShadowAtlasQuadTree {
+public:
+	void Add();
+	void Clear();
+
+private:
+	TArray<Node> Nodes;
+	
+};
