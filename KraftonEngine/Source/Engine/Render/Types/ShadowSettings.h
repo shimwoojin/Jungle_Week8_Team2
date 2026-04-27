4b28731c3c0aa6a6d26ffadb11117d7dd882c72d
LEE SangHoon <hoon15x@gmail.com>
2026-04-27 14:35:49 +0900

    hotfix: resolution setting
    ShadowSettings.h) kDefaultResolution 32 -> 2048

--- KraftonEngine/Source/Engine/Render/Types/ShadowSettings.h
+++ KraftonEngine/Source/Engine/Render/Types/ShadowSettings.h
@@ -59,7 +59,7 @@ public:
 	}
 
 	// 기본값 상수
-	static constexpr uint32 kDefaultResolution = 32;
+	static constexpr uint32 kDefaultResolution = 2048;
 	static constexpr float  kDefaultBias = 0.005f;
 	static constexpr float  kDefaultSlopeBias = 0.005f;
 	static constexpr EShadowFilterMode kDefaultFilterMode = EShadowFilterMode::Hard;
