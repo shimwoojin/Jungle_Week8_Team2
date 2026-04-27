7818b3790f44db91f4040903cebf0dec33a1e1af
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-04-27 17:14:45 +0900

    Add AtlasQuadTreeBase cpp implementations

--- KraftonEngine/Source/Engine/Render/Shadow/AtlasQuadTreeBase.h
+++ KraftonEngine/Source/Engine/Render/Shadow/AtlasQuadTreeBase.h
@@ -15,12 +15,14 @@ struct Node {
 
 struct FAtlasRegion { uint32 X, Y, Size; bool bValid; };
 
+struct FPointLightParams;
+
 class FAtlasQuadTreeBase {
 public:
 	// Initializes the head node of the atlas, and define the minimum resolution for the shadow maps to be allocated.
 	virtual void Init(float InAtlasSize, float inMinShadowMapResolution) = 0;
 
-	virtual void AddToBatch(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) = 0;
+	virtual void AddToBatch(const FPointLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) = 0;
 
 	// Sorts the pending batch by evaluated resolution, then allocates all entries into the atlas.
 	virtual TArray<FAtlasRegion> CommitBatch() = 0;
@@ -39,7 +41,7 @@ protected:
 	virtual FAtlasRegion AllocateNode(int32 NodeIdx, uint32 RequestedSize) = 0;
 
 	// Ranks the importance of the input light source based on its properties.
-	virtual float EvaluateResolution(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) const = 0;
+	virtual float EvaluateResolution(const FPointLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) const = 0;
 
 	// Greedily splits the quadtree to find the best fit for the new shadow map
 	bool  Split(int32 Idx);
